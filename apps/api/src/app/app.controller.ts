d0763b5641af6eb7dd947b69798c36bd9a1182a4
Angelo Chantziantoniou <angelo.chantziantoniou@gov.ab.ca>
2021-11-25 10:10:34 -0700

    git settings

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -2,13 +2,25 @@
 import { Bind, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import * as crypto from 'crypto';
 import multer = require('multer');
 
 import { AppService } from './app.service';
 import { FileCompressionInterceptor } from './interceptors/file-compression.interceptor';
 
+const FILE_DIR = './files';
 export const multerOptions = {
-  dest: './files',
+  dest: FILE_DIR,
+  storage: multer.diskStorage({
+    destination: FILE_DIR,
+    filename: (req, file, cb) => {
+      const ext = file.originalname.substring(
+        file.originalname.lastIndexOf('.', file.originalname.length)
+      );
+      const name = crypto.randomBytes(10).toString('hex');
+      cb(null, name + ext);
+    },
+  }),
 } as MulterOptions;
 
 @Controller('file')
